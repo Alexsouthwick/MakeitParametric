ac5d40e9dec4ca652d0d98e4eab943a08f086853
Alexsouthwick <acsouthwick@gmail.com>
2016-10-13 09:46:22 -0700

    added circle and params

--- makeitparametric.js
+++ makeitparametric.js
@@ -1,7 +1,11 @@
 function getParameterDefinitions(){
-  return[(name: 'width', type: 'float', ititial: 12, caption: 'Circumference in cm' )]  
+return [
+    {name: 'width', type: 'float', initial: 10, caption: "Curcumference in cm:"},
+    {name: 'height', type: 'float', initial: 2, caption: "Height in cm:"},
+    {name: 'height_of_design', type: 'float', initial: .5, caption: "Height of Design:"},
+    ]; 
 }
 
 function main(params) {
-    return rectangle([1,2,3]);
+    return square([params.height, params.width]).subtract(circle(params.height_of_design));
 }
