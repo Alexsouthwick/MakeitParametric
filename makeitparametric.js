99f03de130005ad828583276f91ea48f132d2d88
Alexsouthwick <acsouthwick@gmail.com>
2016-10-19 14:27:20 -0700

    added stuff

--- makeitparametric.js
+++ makeitparametric.js
@@ -3,9 +3,16 @@ return [
     {name: 'width', type: 'float', initial: 10, caption: "Curcumference in cm:"},
     {name: 'height', type: 'float', initial: 2, caption: "Height in cm:"},
     {name: 'height_of_design', type: 'float', initial: .5, caption: "Height of Design:"},
+    {name: 'number_design', type: 'float', initial: 4, caption: "Number of Design"}
     ]; 
 }
 
 function main(params) {
-    return square([params.height, params.width]).subtract(circle(params.height_of_design));
+    var design= [];
+    for (i = 0; i<params.number_design; i++){
+        design.push(circle(params.height_of_design))
+
+    }
+    var bracelet= square([params.height, params.width]).subtract(circle(params.height_of_design));
+    return bracelet;
 }
